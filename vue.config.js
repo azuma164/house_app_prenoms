71d105ac95cd7d93f27228fc58616f521cf3ebc6
Azuma Hiroki <ornitho1027@gmail.com>
2021-12-22 09:14:08 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/prenoms-app/'   //任意
-      : '/'
+    // publicPath: process.env.NODE_ENV === 'production'
+    publicPath: "/",
+    outputDir: "docs"
   }
